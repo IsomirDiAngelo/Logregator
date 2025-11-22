aa6ec1183791a627b12d0a14e439f00cb1841506
IsomirDiAngelo <isomirdiangelo@gmail.com>
2025-11-22 12:12:05 +0100

    Fix DB structure

--- logregator_backend/api/models/api_token.py
+++ logregator_backend/api/models/api_token.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django.db import models
 
 class APIToken(models.Model):
-    content = models.CharField
-    created_at = models.DateTimeField
-    updated_at = models.DateTimeField
+    content = models.CharField(null=True)
+    created_at = models.DateTimeField(default=datetime.now())
+    updated_at = models.DateTimeField(default=datetime.now())
--- logregator_backend/api/models/collector.py
+++ logregator_backend/api/models/collector.py
@@ -1,7 +1,8 @@
 from django.db import models
+from datetime import datetime
 from .api_token import APIToken
 
 class Collector(models.Model):
     name = models.CharField(max_length=10)
     api_token = models.ForeignKey(APIToken, on_delete=models.CASCADE)
-    created_at = models.DateTimeField
+    created_at = models.DateTimeField(default=datetime.now())
